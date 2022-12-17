a03b2dbb2600c1d8d659c49d6132a70182c500af
GudkovSergey <gudkow-sergey@mail.ru>
2022-12-17 15:50:26 +0500

    put the logo

---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import style from './Header.module.scss';
+import logo from './../assets/logoLight.png';
+
+const Header = (props: any) => {
+    return (
+
+
+        <div className={style.header_container}>
+
+            <div className={style.header_logo}>
+                <img src={logo} alt=""/>
+            </div>
+
+            <ul className={style.header_menu}>
+
+                    <li ><a href="">Home</a></li>
+                    <li ><a href="">About me </a></li>
+                    <li ><a href="">My skills</a></li>
+                    <li ><a href="">Project</a></li>
+                    <li ><a href="">Contacts</a></li>
+                    <li ><a href="">Ru</a></li>
+
+            </ul>
+
+        </div>
+
+    )
+
+}
+
+export default Header;
---
+++ my-app/src/components/HeaderContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+
+
+const HeaderContainer = (props:any) =>{
+
+        return(
+            <Header{...props}/>
+        )
+
+}
+export default HeaderContainer;
